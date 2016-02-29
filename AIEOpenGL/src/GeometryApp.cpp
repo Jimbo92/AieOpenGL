7e82615e61229c44f2c693edb5db3f52c02325ef
James Stone <james.stone@ad.aie.edu>
2016-02-29 14:23:02 +1100

    small updates

--- AIEOpenGL/src/GeometryApp.cpp
+++ AIEOpenGL/src/GeometryApp.cpp
@@ -80,114 +80,6 @@ bool GeometryApp::startup()
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 	//===================//Load OBJ models//==============================//
-	
-	/*
-	Texture* earthTexture = new Texture("./data/ruinedtank/ground_diff.jpg");
-	Shader* CoolShader = new Shader("./data/Shaders/vs_texture_wave.vert", "./data/Shaders/fs_texture.frag", earthTexture);
-	CoolShader->m_light = m_testLight;
-
-	Shader* NormalShader = new Shader("./data/vs_standardmodel.vert", "./data/fs_standardmodel.frag");
-
-	Texture* groundText = new Texture("./data/ruinedtank/ground_diff.jpg");
-	Shader* groundShader = new Shader("./data/Shaders/vs_texture.vert", "./data/Shaders/fs_texture.frag", groundText);
-	groundShader->m_specpow = 1.1f;
-	groundShader->m_light = m_testLight;
-
-	Texture* engineText = new Texture("./data/ruinedtank/left_engine_diff.jpg");
-	Texture* engineText_N = new Texture("./data/ruinedtank/left_engine_norm.jpg");
-	Shader* engineShader = new Shader("./data/Shaders/vs_texture.vert", "./data/Shaders/fs_texture_norm.frag", engineText, engineText_N);
-	engineShader->m_light = m_testLight;
-
-	Texture* turretText = new Texture("./data/ruinedtank/turret_diff.jpg");
-	Texture* turretText_N = new Texture("./data/ruinedtank/turret_norm.jpg");
-	Shader* turretShader = new Shader("./data/Shaders/vs_texture.vert", "./data/Shaders/fs_texture_norm.frag", turretText, turretText_N);
-	turretShader->m_light = m_testLight;
-
-	BunnyModel = new Model("./data/ruinedtank/tank");
-	BunnyModel->ModelShaders.push_back(groundShader);
-	BunnyModel->ModelShaders.push_back(engineShader);
-	BunnyModel->ModelShaders.push_back(groundShader);
-	BunnyModel->ModelShaders.push_back(groundShader);
-	BunnyModel->ModelShaders.push_back(groundShader);
-	BunnyModel->ModelShaders.push_back(groundShader);
-	BunnyModel->ModelShaders.push_back(engineShader);
-	BunnyModel->ModelShaders.push_back(engineShader);
-	BunnyModel->ModelShaders.push_back(turretShader);
-	BunnyModel->ModelShaders.push_back(turretShader);
-
-	LucyModel = new Model("./data/Lucy", 0, glm::vec3(10,0,0), glm::vec3(0.1,0.1,0.1));
-
-
-	Texture* SwordTexture = new Texture("./data/soulspear/soulspear_diffuse.tga");
-	Texture* SwordTexture_N = new Texture("./data/soulspear/soulspear_normal.tga");
-	Texture* SwordTexture_Spec = new Texture("./data/soulspear/soulspear_specular.tga");
-	Shader* SwordShader = new Shader("./data/Shaders/vs_texture.vert", "./data/Shaders/fs_texture_norm_spec.frag", SwordTexture, SwordTexture_N, SwordTexture_Spec);
-	SwordShader->m_light = m_testLight;
-	SwordShader->m_specpow = 1.2f;
-	SwordModel = new Model("./data/soulspear/soulspear", 0, vec3(0, 20, 0));
-	SwordModel->ModelShaders.push_back(SwordShader);
-	
-	//water
-	Texture* WaterTexture = new Texture("./data/water/water_diffuse.jpg");
-	Texture* WaterTexture_Norm = new Texture("./data/water/water_normal.jpg");
-	WaterShader = new Shader("./data/Shaders/vs_texture_wave.vert", "./data/Shaders/fs_texture_wave.frag", WaterTexture, WaterTexture_Norm);
-	WaterShader->m_light = m_testLight;
-	WaterShader->m_alpha = 0.5f;
-	WaterShader->m_specpow = 10.0f;
-	WaterModelTest = new Model("./data/water/waterplane", 0, vec3(5, 0.3f, -3));
-	WaterModelTest->ModelShaders.push_back(WaterShader);
-
-	//grass
-	Texture* GrassTexture = new Texture("./data/grass/grass_diff.tga");
-	Texture* GrassTexture_N = new Texture("./data/grass/grass_norm.tga");
-	Texture* GrassTexture_Spec = new Texture("./data/grass/grass_spec.tga");
-	Shader* GrassShader = new Shader("./data/Shaders/vs_grass.vert", "./data/Shaders/fs_texture_norm_spec.frag", GrassTexture, GrassTexture_N, GrassTexture_Spec);
-	GrassShader->m_light = m_testLight;
-	GrassShader->m_specpow = 1.1f;
-	mdl_GrassChunk1 = new Model("./data/grass/Grass_01", 0, vec3(-5.f, 6.6f, 18.f));
-	mdl_GrassChunk1->ModelShaders.push_back(GrassShader);
-	mdl_GrassChunk2 = new Model("./data/grass/Grass_02", 0, vec3(-5.f, 6.5f, 18.f), glm::vec3(0.5f, 0.5f, 0.5f));
-	mdl_GrassChunk2->ModelShaders.push_back(GrassShader);
-	mdl_GrassChunk3 = new Model("./data/grass/Grass_03", 0, vec3(-5.f, 6.6f, 18.f));
-	mdl_GrassChunk3->ModelShaders.push_back(GrassShader);
-
-	//palmTree
-	Texture* PalmTreeTrunkTexture = new Texture("./data/palmtree/ENV_MP_Iraq_palm_tree_01_D.tga");
-	Texture* PalmTreeTrunkTexture_N = new Texture("./data/palmtree/ENV_MP_Iraq_palm_tree_01_N.tga");
-	Shader* PalmTreeTrunkShader = new Shader("./data/Shaders/vs_texture.vert", "./data/Shaders/fs_texture_norm.frag", PalmTreeTrunkTexture, PalmTreeTrunkTexture_N);
-	PalmTreeTrunkShader->m_light = m_testLight;
-
-	Texture* PalmTreeBranchTexture = new Texture("./data/palmtree/ENV_MP_Iraq_PlantsSansTrans_D.tga");
-	Texture* PalmTreeBranchTexture_Spec = new Texture("./data/palmtree/ENV_MP_Iraq_PlantsSansTrans_S.tga");
-	Shader* PalmTreeBranchShader = new Shader("./data/Shaders/vs_branch.vert", "./data/Shaders/fs_texture_norm_spec.frag", PalmTreeBranchTexture, nullptr, PalmTreeBranchTexture_Spec);
-	PalmTreeBranchShader->m_light = m_testLight;
-	PalmTreeBranchShader->m_specpow = 1.f;
-
-	mdl_PalmTree = new Model("./data/palmtree/Palm_Tree", 0, vec3(-5.f, 6.6f, 18.f), glm::vec3(0.1f, 0.1f, 0.1f));
-	mdl_PalmTree->m_RotAxis = glm::vec3(1, 0, 0);
-	mdl_PalmTree->m_RotAmount = glm::radians(-90.f);
-	mdl_PalmTree->ModelShaders.push_back(PalmTreeBranchShader);
-	mdl_PalmTree->ModelShaders.push_back(PalmTreeBranchShader);
-	mdl_PalmTree->ModelShaders.push_back(PalmTreeBranchShader);
-	mdl_PalmTree->ModelShaders.push_back(PalmTreeTrunkShader);
-	*/
-
-	//Texture* pyro_Texture = new Texture("./data/characters/Enemytank/EnemyTank_D.tga");
-	//Texture* pyro_Texture_N = new Texture("./data/characters/Enemytank/EnemyTank_N.tga");
-	//Texture* pyro_Texture_S = new Texture("./data/characters/Enemytank/EnemyTank_S.tga");
-	//
-	//Texture* pyroGun_Texture = new Texture("./data/characters/Pyro/Flamethrower_D.png");
-	//Texture* pyroGun_Texture_N = new Texture("./data/characters/Pyro/Flamethrower_N.png");
-	//Texture* pyroGun_Texture_S = new Texture("./data/characters/Pyro/Flamethrower_S.png");
-	//
-	//Shader* pyro_Shader = new Shader("./data/Shaders/vs_animation.vert", "./data/Shaders/fs_texture_norm_spec.frag", pyro_Texture, pyro_Texture_N, pyro_Texture_S);
-	//pyro_Shader->m_light = m_testLight;
-	//pyro_Shader->m_specpow = 0.5f;
-	//
-	//Shader* pyroGun_Shader = new Shader("./data/Shaders/vs_animation.vert", "./data/Shaders/fs_texture_norm_spec.frag", pyroGun_Texture, pyroGun_Texture_N, pyroGun_Texture_S);
-	//pyroGun_Shader->m_light = m_testLight;
-	//pyroGun_Shader->m_specpow = 1.5f;
-
 
 	mdl_Sponza = new Model("./data/characters/Marksman/Marksman.fbx", 1, true, glm::vec3(0), glm::vec3(0.003f, 0.003f, 0.003f));
 	mdl_Sponza->ModelShaders[0]->m_light = m_testLight;
@@ -325,11 +217,6 @@ void GeometryApp::draw()
 	{
 		//setup fbo render
 		glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
-		glViewport(0, 0, 1280, 720);
-	}
-	else
-	{
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		glViewport(0, 0, 1280, 720);
 	}
 
